e4ebdc1a5e977beb0fb45750a04e4ded0dd1309d
tankedshoe <tankedshoe@gmail.com>
2018-04-30 08:53:28 -0600

    Moved some files/ established a MVC setup.

--- daneGame/Controller/Program.cs
+++ daneGame/Controller/Program.cs
@@ -11,7 +11,7 @@ using UIKit;
 #endif
 #endregion
 
-namespace daneGame
+namespace daneGame.Controller
 {
 #if __IOS__ || __TVOS__
     [Register("AppDelegate")]
--- daneGame/Controller/SpaceGame.cs
+++ daneGame/Controller/SpaceGame.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace daneGame
+namespace daneGame.Controller
 {
 	/// <summary>
 	/// This is the main type for your game.
